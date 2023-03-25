072ba8cc689b285f6cf367c62b0b25a1059ca900
ZiadSheriif <zsherif308@gmail.com>
2023-03-25 02:23:01 +0200

    docs: update documentations

--- src/Layouts/ShowTasks/ShowTasks.jsx
+++ src/Layouts/ShowTasks/ShowTasks.jsx
@@ -1,9 +1,17 @@
+// Imports
 import React from "react";
 import CardTask from "Components/CardTask/CardTask";
 import { ContainerTasks } from "Pages/HomePage/HomePage.styled";
 
-// Define a new React component called ShowTasks
-// This component takes three props: filteredTasks, setTasks, and viewTask
+/**
+ * Renders a list of tasks based on the `filteredTasks` prop passed to it.
+ * @param {Object} props - The component props.
+ * @param {Array} props.filteredTasks - The list of tasks to render.
+ * @param {Function} props.setTasks - The function to update the list of tasks.
+ * @param {Function} props.viewTask - The function to display a single task.
+ * @returns {React.Component} A React component that displays a list of tasks.
+ */
+
 const ShowTasks = ({ filteredTasks, setTasks, viewTask }) => {
   return (
     <>
